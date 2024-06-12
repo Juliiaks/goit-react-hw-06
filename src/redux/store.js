4f9fd7db94a516c207122a4250cb6ef94eda50d2
Juliiaks <juliakharetonchuk@gmail.com>
2024-06-12 22:24:50 +0300

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  contacts: {
+		items: []
+	},
+  filters: {
+		name: ""
+	}
+}
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+export const store = createStore(rootReducer);
